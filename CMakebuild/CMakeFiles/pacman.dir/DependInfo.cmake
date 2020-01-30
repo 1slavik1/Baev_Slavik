54baa049edaf75ed765acfd792f9fe9508d095fb
1slavik1 <42204152+1slavik1@users.noreply.github.com>
2020-01-31 00:29:59 +0200

    Add files via upload

---
+++ CMakebuild/CMakeFiles/pacman.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/C++/GAME/PacMan/newpacman/Main/Blinki.cpp" "D:/C++/GAME/PacMan/newpacman/Main/build/CMakeFiles/pacman.dir/Blinki.cpp.obj"
+  "D:/C++/GAME/PacMan/newpacman/Main/Cursor.cpp" "D:/C++/GAME/PacMan/newpacman/Main/build/CMakeFiles/pacman.dir/Cursor.cpp.obj"
+  "D:/C++/GAME/PacMan/newpacman/Main/Draw.cpp" "D:/C++/GAME/PacMan/newpacman/Main/build/CMakeFiles/pacman.dir/Draw.cpp.obj"
+  "D:/C++/GAME/PacMan/newpacman/Main/Game.cpp" "D:/C++/GAME/PacMan/newpacman/Main/build/CMakeFiles/pacman.dir/Game.cpp.obj"
+  "D:/C++/GAME/PacMan/newpacman/Main/Ghost.cpp" "D:/C++/GAME/PacMan/newpacman/Main/build/CMakeFiles/pacman.dir/Ghost.cpp.obj"
+  "D:/C++/GAME/PacMan/newpacman/Main/Main.cpp" "D:/C++/GAME/PacMan/newpacman/Main/build/CMakeFiles/pacman.dir/Main.cpp.obj"
+  "D:/C++/GAME/PacMan/newpacman/Main/Map.cpp" "D:/C++/GAME/PacMan/newpacman/Main/build/CMakeFiles/pacman.dir/Map.cpp.obj"
+  "D:/C++/GAME/PacMan/newpacman/Main/Menu.cpp" "D:/C++/GAME/PacMan/newpacman/Main/build/CMakeFiles/pacman.dir/Menu.cpp.obj"
+  "D:/C++/GAME/PacMan/newpacman/Main/Pac.cpp" "D:/C++/GAME/PacMan/newpacman/Main/build/CMakeFiles/pacman.dir/Pac.cpp.obj"
+  "D:/C++/GAME/PacMan/newpacman/Main/Timer.cpp" "D:/C++/GAME/PacMan/newpacman/Main/build/CMakeFiles/pacman.dir/Timer.cpp.obj"
+  "D:/C++/GAME/PacMan/newpacman/Main/pch.cpp" "D:/C++/GAME/PacMan/newpacman/Main/build/CMakeFiles/pacman.dir/pch.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
